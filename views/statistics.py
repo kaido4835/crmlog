6ab55684e7d9553721e75a8890f7f11187f4fa41
mironshoh <rahmatullaevmironshoh@gmail.com>
2025-04-24 13:11:29 +0500

    commit

--- views/statistics.py
+++ views/statistics.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request, send_file
 from flask_login import login_required, current_user
 from datetime import datetime, timedelta
-from sqlalchemy import func, distinct, case, extract
+from sqlalchemy import func, distinct, case, extract, or_
 
 from app import db
 from models import (
